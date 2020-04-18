b53f03deb5baff8d3f5a3700e864477ad6396700
Luiz Jacó <>
2020-04-18 17:09:20 -0300

    Creating detector_page

--- pet_detector/lib/main.dart
+++ pet_detector/lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'package:pet_detector/pages/detector_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pet Detector',
       theme: ThemeData.dark(),
-      home: Container(),
+      home: DetectorPage(),
     );
   }
 }
---
+++ pet_detector/lib/pages/detector_page.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+
+class DetectorPage extends StatefulWidget {
+  @override
+  _DetectorPageState createState() => _DetectorPageState();
+}
+
+class _DetectorPageState extends State<DetectorPage> {
+  bool _isLoading;
+  File _file;
+  List _output;
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = true;
+    loadModel().then((value) {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  Future<String> loadModel() async {
+    return await Tflite.loadModel(
+      model: 'assets/model_unquant.tflite',
+      labels: 'assets/labels.txt',
+    );
+  }
+
+  Future chooseFile() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return null;
+
+    setState(() {
+      _isLoading = true;
+      _file = image;
+    });
+
+    classifyImage(image);
+  }
+
+  Future classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+
+    Future.delayed(Duration(milliseconds: 250)).then((_) {
+      setState(() {
+        _isLoading = false;
+        _output = output;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pet Detector'),
+      ),
+      body: Container(
+        child: _isLoading
+            ? const Center(child: const CircularProgressIndicator())
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _file != null ? Image.file(_file) : Container(),
+                  const SizedBox(height: 8.0),
+                  _output != null
+                      ? Chip(
+                          backgroundColor: Theme.of(context).accentColor,
+                          label: Text(
+                            'It\'s a ${_output[0]['label']
+                                .toString()
+                                .substring(2)
+                                }'.toUpperCase(),
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                      : const Text('Pick an image on your gallery'),
+                ],
+              ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add_photo_alternate),
+        onPressed: chooseFile,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+}
